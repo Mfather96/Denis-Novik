c87d988a3536bf10ed4dd614dafe6b53c433668e
Mfather96 <vladimirpro9696@gmail.com>
2022-10-18 20:58:56 +0300

    done app

--- js/main.js
+++ js/main.js
@@ -4,6 +4,10 @@
 	const LinkNav = document.querySelectorAll('.link-nav');
 	const headerMenu = document.querySelector('.header-menu');
 	const headerLinks = document.querySelector('.header_links');
+	const messageBtn = document.querySelector('.block-button-send-message');
+	const closeFormMessage = document.querySelector('.form-message_close-icon');
+	const formMessage = document.querySelector('.contacts__form-message');
+	const sendMessage = document.querySelector('.message_send-button');
 
 	btnOpenMenu.addEventListener('click', ()=>{
 		headerMenu.classList.toggle('open-menu');
@@ -19,3 +23,31 @@
 			}
 		}
 	)})
+
+messageBtn.addEventListener('click', ()=>{
+		if(!formMessage.classList.contains('active')){
+			formMessage.classList.add('active');
+		}
+})
+
+closeFormMessage.addEventListener('click', ()=>{
+		if(formMessage.classList.contains('active')){
+			formMessage.classList.remove('active');
+		}
+})
+
+sendMessage.addEventListener('click', sendMessageFun)
+
+function sendMessageFun(){
+	if(!document.querySelector('.form-message_message-sended').classList.contains('active') && formMessage.classList.contains('active')){
+	 document.querySelector('.form-message_message-sended').classList.add('active');
+	formMessage.classList.remove('active');
+	setTimeout(closeSendMessage, 900);
+	}
+}
+
+function closeSendMessage(){
+	if(document.querySelector('.form-message_message-sended').classList.contains('active')){
+		document.querySelector('.form-message_message-sended').classList.remove('active');
+	}
+}
